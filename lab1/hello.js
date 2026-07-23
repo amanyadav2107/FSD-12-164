9278171291d855334b20dfa5e362ea89533cb944
Aman Yadav <amanyadavchak@gmail.com>
2026-07-23 11:53:55 +0530

    Inital Commit

---
+++ lab1/hello.js
@@ -0,0 +1,28 @@
+// console.log("Aman Yadav")
+
+//Write a function to take two number and return their sum
+
+// function add(a,b){
+//     let sum=a+b;
+//     return sum;
+// }
+
+// console.log(add(2,2));
+
+
+// Older Function
+function sum(a,b){
+    return a+b;
+}
+
+//Arrow fun. method
+const add = (a,b) => {
+    return a+b;
+};
+
+Adv arrow function
+const add2 =(a,b) => a+b;
+
+console.log(sum(20,10));
+console.log(sum(20,10));
+console.log(sum(20,10));
