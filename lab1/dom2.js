00e8c0c9e75f694d2a4662ab4151172fe1253d65
Aman Yadav <amanyadavchak@gmail.com>
2026-07-30 11:05:05 +0530

    Even Emitter

---
+++ lab1/dom2.js
@@ -0,0 +1,23 @@
+// import {EventEmitter } from "events"; 
+
+// const music =new EventEmitter();
+
+// music.on("click" ,() => {
+//     console.log("Music ");
+// });
+
+// music.emit("click");
+
+import {EventEmitter } from "events"; //Event emitter kisi v event ko generate krta hai
+
+const button =new EventEmitter();
+
+button.on("click" ,() => {
+    console.log("Task 1 ");
+});
+
+button.on("click" ,() => {
+    console.log("Task 1 ");
+});
+
+button.emit("click");
