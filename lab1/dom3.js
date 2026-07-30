af3f2754d9dcc538787221a336db8c17aaaeb71e
Aman Yadav <amanyadavchak@gmail.com>
2026-07-30 11:10:20 +0530

    Event Emitter

---
+++ lab1/dom3.js
@@ -0,0 +1,12 @@
+import {EventEmitter } from "events";
+
+const button = new EventEmitter();
+
+button.on("click",(uname) =>{
+    console.log(`button clicked by ${uname}`);
+});
+
+button.emit("click", "Aman");
+button.emit("click", "Amey");
+button.emit("click", "Rani");
+button.emit("click");
