92a10a9c634441b4c3bc7a70a016aca40cd63101
Aman Yadav <amanyadavchak@gmail.com>
2026-08-20 12:22:59 +0530

    Crud

--- lab2/crud.js
+++ lab2/crud.js
@@ -9,10 +9,10 @@ const getCart = async() => {
     return JSON.parse(data);
 
 }
-
 const saveCart = async (myCart) => {
     await writeFile(FILE, JSON.stringify(myCart, null, 2));
 };
+
 const addTocart = async (product) => {
     const myCart = await getCart();
     const isFound = myCart.find((item) => item.id === product.id);
@@ -26,9 +26,53 @@ const addTocart = async (product) => {
 };
 
 const ShowCart = async () => {
-    const myCart = await getCart();
-    console.table(myCart);
+    const data = await getCart();
+    console.table(data);
+    let total = data.reduce((t, item)=> t + item.qty* item.price,0)
+    // let total =0;
+    // for(let i=0; i<data.length; i++){
+    //     total=total + data[i].qty * data[i].price;
+    // }
+    console.log("You have to pay: ", total)
 };
+
+const count = data.length;
+const newData = data.filter((item)=> item.id ! ==pid);
+const newCount = newdata.length;
+if(count == new count){
+    console.log("pid not found");
+}
+else{
+    await saveCart(new data);
+    console.log("Product deleted succesfully");
+}
+const updateCart = async(pid, value)=>{
+    const data = await getCart();
+    const isFound = data.find((item)=> item.id === pid);
+    if(isFound){
+        isFound.qty += value;
+        awit saveCart(data);
+        console.log("product quantit updated successfullyy");
+    } else{
+        console.log("Product id not found");
+    }
+    }
+};
+
+const removeFromCart = async(pid) =>{
+    const myCart = await getCart();
+    const count = myCart.length;
+    const newData = myCart.filter((item)=> item.id !== pid);
+    const newCount = newData.length;
+    if(count == newCount){
+        console.log(`Product with id ${pid} not found`);
+    }
+    else{
+        await saveCart(newData);
+        console.log(`Product with id ${pid} deleted successfully`);
+    }
+};
+
 
 
 const main = async ()=>{
@@ -54,10 +98,13 @@ const main = async ()=>{
 
             break;
         case 3:
-            console.log("remove product");
+            let pid = await cin.question("Enter product id to remove: ");
+            await removeFromCart(Number(pid));
             break;
         case 4:
-            console.log("Update product quantity");
+            let pid2 = await cin.question("Enter product id to update:");
+            let value = await cin.question("+1 increse, -1 decrese:");
+            await updateCart(Number(pid),Number(value));
             break;
         case 5:
             console.log("See you later");
