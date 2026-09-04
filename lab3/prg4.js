a4fd3e79d6a66b14c820960a1852f000afb19fc3
Aman Yadav <amanyadavchak@gmail.com>
2026-09-04 13:26:31 +0530

    Prg 4

--- lab3/prg4.js
+++ lab3/prg4.js
@@ -1,7 +1,7 @@
 import http from 'http';
 
  const server = http.createServer((req,res) => {
-     res.writeHead(200,{'conttent-type': 'apllication/json'});
+     res.writeHead(200,{'conttent-type': 'text/json'});
      console.log("Client URL;",req.url);
 
 
@@ -17,10 +17,16 @@ import http from 'http';
       qty:1,
       discount: 25,
      },
+     {
+        name: "Mouse",
+        price: 2500,
+        qty:2,
+        discount: 20,
+     },
     ];
 
      res.end(JSON.stringify(product));
  });
 
 
- server.listen(3000,() => console.log("server is runnig at 3000...."));
+ server.listen(3000,() => console.log("prg4 is runnig at 3000...."));
